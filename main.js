d6bac57199311654c71f67b6455b8dfc0222300f
sharmin231 <sharminaktersabinaafrin@gmail.com>
2024-09-09 20:22:46 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,105 @@
+const btn1 = document.querySelector("#btn1")
+const btn2 = document.querySelector("#btn2")
+const submit = document.querySelector("#submit")
+const reset = document.querySelector("#reset")
+const winningPlayer = document.querySelector("#winner")
+const wScore = document.querySelector("#score")
+const input = document.querySelector("#input")
+const player1 = document.querySelector("#payer1")
+const player2 = document.querySelector("#payer2")
+
+// let score = 0;
+let winningScore = 5;
+let p1Elm = 0;
+let p2Elm = 0;
+let gameOver = false;
+let p1Turn = true;
+let p2Turn = false;
+
+
+// function updateScore() {
+//     return score++}
+
+
+function disabledButton() {
+    if (p1Elm === winningScore || p2Elm === winningScore) {
+        // gameOver = true;
+
+        btn1.setAttribute("disabled", "disabled")
+        btn2.setAttribute("disabled", "disabled")
+    }
+}
+// function disable() {
+//     btn1.setAttribute("disabled", "disabled")
+//     btn2.setAttribute("disabled", "disabled")
+    
+// }
+
+function identifyWinner() {
+    if (p1Elm === winningScore) {
+        winningPlayer.textContent="player- 1 is winner !!"
+    } else if (p2Elm === winningScore) {
+        winningPlayer.textContent="player- 2 is winner !!"
+    }
+}
+
+wScore.textContent = winningScore;
+btn1.addEventListener("click", (e) => {
+    if (p1Turn) {
+        p1Elm++
+    player1.textContent = p1Elm;
+    identifyWinner()
+    // disabledButton()
+    
+        p1Turn = false
+        p2Turn = true
+        btn1.setAttribute("disabled", "disabled")
+        btn2.removeAttribute("disabled")
+    }
+    // if (gameOver) {
+    //     disable()
+    //     }
+    disabledButton()
+})
+
+btn2.addEventListener("click", (e) => {
+    if (p2Turn) {
+        p2Elm++
+    player2.textContent = p2Elm;
+    identifyWinner()
+    // disabledButton()
+        p2Turn = false
+        p1Turn = true
+        btn2.setAttribute("disabled", "disabled")
+        btn1.removeAttribute("disabled")
+    }
+    // if (gameOver) {
+    //     disable()
+    //     }
+    disabledButton()
+
+})
+
+
+
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    const inputValue = document.getElementById("input").value;
+    console.log(inputValue)
+    wScore.textContent = inputValue;
+
+
+})
+reset.addEventListener("click",(e)=> {
+    winningScore = 5;
+    p1Elm = 0;
+    p2Elm = 0;
+    gameOver = false;
+
+    btn1.removeAttribute("disabled")
+    btn2.removeAttribute("disabled")
+    player1.textContent = p1Elm;
+    player2.textContent = p2Elm;
+    winningPlayer.textContent= ''
+})
